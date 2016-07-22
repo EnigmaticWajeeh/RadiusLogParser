cb5a902263fd16358ad83ef7b0b63d2e022cf89a
EnigmaticWajeeh <enigmaticwajeeh@gmail.com>
2016-07-22 12:48:07 +0500

    cassandra integrated

--- src/RadiusLogParser.java
+++ src/RadiusLogParser.java
@@ -9,25 +9,36 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 /* copyright wajeeh */
 
 public class RadiusLogParser {
 
-	static boolean loginOK;
-	static boolean loginIncorrect;
-	static String macAddress;
-	static String userName;
-	static String date;
-	static String month;
-	static String year;
-	static String time;
-	static String day;
-	private static String type;
-	private static Integer loginAttempts;
-	private static Object infoDesc;
+	
 
 	public static void main(String[] args) {
 
+		boolean loginOK;
+		boolean loginIncorrect;
+		String macAddress;
+		String userName;
+		String date;
+		String month;
+		String year;
+		String time;
+		String day;
+		String type;
+		Integer loginAttempts;
+		String infoDesc;
+		boolean error;
+		
+		
+		
+		// Initial Setup for parser
+		
 		ArrayList<String> inputArguments=new ArrayList<String>();
 		
 		for(int i=0; i < args.length; i++){
@@ -42,8 +53,76 @@ public class RadiusLogParser {
 		
 		File firstFile=new File(inputArguments.get(0));
 		
-		// FileReader firstFileRead=);
-		// new FileReader(inputArguments.get(0))
+		// intial setup for parser done, read first argument file and start parsing
+		
+		
+		
+		
+		
+		
+//		CONNECTION PROPERTIES FOR APACHE CASSANDRA
+		
+			String serverIp = "127.0.0.1";
+
+		    String keyspace = "log";
+		    String tableName="radius";
+		    
+		    Cluster cluster = Cluster.builder()
+		            .addContactPoints(serverIp)
+		            .build();
+
+		    String createKeyspaceString="CREATE KEYSPACE IF NOT EXISTS " + keyspace + " " +
+		    "with " + "replication = {'class' : 'SimpleStrategy', 'replication_factor' : 1}";
+		    
+		    String createTableString= "CREATE TABLE IF NOT EXISTS "+ tableName +
+		    "( event_id timeuuid primary key,"+
+		    " loginok boolean, " +
+		    "loginIncorrect boolean, " +  
+		    "error boolean, " +
+		    "type text, " + 
+		    "macAddress text, " +
+		    "userName text, " +
+		    "year text, " +
+		    "month text, " +
+		    "date text," +
+		    "time text, " + 
+		    "day text, " +
+		    "loginAttempts int," +
+		    "description text, " +
+		    ")" ;
+		    
+		    		/*
+		    		 * insert into radius (loginok, loginIncorrect, macAddress,
+		    		 * userName, date, month, year, time, day, type, loginAttempts,
+		    		 *  description, error) values (now(),);
+		    		 */
+		    
+		    // CONNECTION SETTINGS ENDED
+		    
+		    
+		    
+		    
+		    
+		    // CREATING SESSION, KEYSPACE AND TABLE
+		    
+//		    Session session = cluster.connect(keyspace);
+		    Session session = cluster.connect();
+		    
+		    session.execute(createKeyspaceString);
+		    
+		    session.execute("use " + keyspace);
+		    
+//		    System.out.println(createTableString);
+		    
+		    session.execute(createTableString);
+		    
+		    // DONE CREATING SESSION< KEYSPACE AND TABLE
+		    
+		    
+		    
+		    
+		    
+		//File exists, now start reading from the file
 		
 		if(firstFile.exists()){
 			System.out.println("The first file exists!");
@@ -54,7 +133,7 @@ public class RadiusLogParser {
 				
 				// Reading from the lines
 				
-				boolean error = false;
+				error = false;
 				
 				PrintWriter pr=new PrintWriter("op.txt","UTF-8");
 				
@@ -71,7 +150,6 @@ public class RadiusLogParser {
 					userName=null;
 					error=false;
 					
-					
 					infoDesc=null;
 					date=null;
 					month=null;
@@ -81,15 +159,12 @@ public class RadiusLogParser {
 					type=null;
 					loginAttempts=null;
 					
-					
-					
 					month=currentLine.substring(4,7);
 					// month done
 					date=currentLine.substring(8,10);
 					// date done
 					year=currentLine.substring(20,24);
 					// year done
-					
 					
 					
 					int endOfDate=currentLine.indexOf(" : Info");
@@ -104,6 +179,7 @@ public class RadiusLogParser {
 						}
 						
 					} 
+					
 					
 					type=currentLine.substring(endOfDate+3,endOfDate+7);
 					
@@ -266,6 +342,45 @@ public class RadiusLogParser {
 //					System.out.println("InfoDesc : " + infoDesc);
 					
 					
+					// INSERT INTO TABLE
+					
+					try{
+					if(infoDesc.contains("'")){
+//						System.out.println("info : " + infoDesc);
+//						infoDesc.replace("'", "");
+						
+						infoDesc=infoDesc.replace("'", "");
+						
+						System.out.println("info : " + infoDesc);
+						
+					}}catch(NullPointerException x){
+						System.out.println("error here : " + infoDesc);
+					}
+					
+					// TODO insert query string
+					String insertQueryString = "INSERT INTO radius (event_id, date, day, description, " +
+				    		"error, loginattempts, loginincorrect, loginok, macaddress, month, time," +
+				    		" type, username, year) values "+"(now(), " +"'" + date +"'" + ", " +"'" + day + "'" 
+				    		+ ", " + "'" + infoDesc 
+				    		+ "'" + ", " + error + ", " + loginAttempts + ", " + loginIncorrect
+				    		+ ", " + loginOK + ", "+"'" + macAddress +"'" + ", " +"'" + month +"'"  +
+				    		", " +"'"  + time +"'"  + ", " +"'"  + type +"'"  + ", " + "'"  + userName +"'" 
+				    		+ ", " +"'"   + year +"'"  + ")";
+					    
+						System.out.println(insertQueryString);
+						System.out.println(infoDesc);
+						
+					    System.out.println(" " + loginOK + loginIncorrect + macAddress + userName +
+					    		date + month + year + time + day + type + loginAttempts + infoDesc +
+					    		error );
+					    
+					    System.out.println(insertQueryString);
+					    
+					    session.execute(insertQueryString);
+					
+					    System.out.println("Query executed");
+					// INSERT INTO TABLE DONE
+					
 					
 					pr.println(" type : " + type + " MacAddress : " + macAddress +
 							" userName : " + " " + userName + " " + date + " " +
@@ -274,6 +389,7 @@ public class RadiusLogParser {
 							loginIncorrect +
 							" error : "	+ error  + " Multiple Login Attempts : " +
 							loginAttempts + " Desc : " + infoDesc);
+					
 					
 				}	// while loop ends here
 				pr.close();
@@ -295,6 +411,12 @@ public class RadiusLogParser {
 			System.out.println("The first file does not exists!");
 		}
 		
-	}
+		 String cqlStatement = "SELECT * FROM " + tableName;
+			
+		    for (Row row : session.execute(cqlStatement)) {	    
+		        System.out.println(row.toString());
+		    }
+
+	} // main function ends here
 
 }
